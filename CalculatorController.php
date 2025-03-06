d443f3df988df0244050c0b1099cb718dfffdb0d
Evan25 <evannickolauskurniady@gmail.com>
2025-03-06 07:25:16 +0700

    Update CalculatorController.php

--- CalculatorController.php
+++ CalculatorController.php
@@ -16,24 +16,14 @@ class CalculatorController extends Controller
         $request->validate([
             'angka1' => 'required|numeric',
             'angka2' => 'required|numeric',
-            'operasi' => 'required|in:tambah,kurang,kali,bagi,modulo',
-            ]
-            ,
-            [
-            'angka1.required' => 'Bilangan 1 wajib diisi',
-            'angka1.numeric' => 'Bilangan 1 harus berupa angka',
-            'angka2.required' => 'Bilangan 2 wajib diisi',
-            'angka2.numeric' => 'Bilangan 2 harus berupa angka',
-            'operasi.required' => 'Operator harus diisi',
-            'operasi.in' => 'Operator tidak valid',
-            ]);
+            'operasi' => 'required'
+        ]);
 
-        $angka1 = $request->angka1;
-        $angka2 = $request->angka2;
-        $operasi = $request->operasi;
+        $angka1 = $request->input('angka1');
+        $angka2 = $request->input('angka2');
+        $operasi = $request->input('operasi');
 
-        //$hasil = 0;
-        switch($operasi){
+        switch ($operasi) {
             case 'tambah':
                 $hasil = $angka1 + $angka2;
                 break;
@@ -44,16 +34,18 @@ class CalculatorController extends Controller
                 $hasil = $angka1 * $angka2;
                 break;
             case 'bagi':
-                $hasil =  $angka2 !=0 ? $angka1 / $angka2 : 'Error: Tidak bisa membagi dengan 0';
+                $hasil = $angka2 != 0 ? $angka1 / $angka2 : 'Error: Pembagian oleh nol';
                 break;
             case 'modulo':
-                $hasil = $angka1 % $angka2;
+                $hasil = $angka2 != 0 ? $angka1 % $angka2 : 'Error: Modulo oleh nol';
+                break;
+            case 'pangkat':
+                $hasil = pow($angka1, $angka2); 
                 break;
             default:
-                $hasil = 'operasi tidak valid';
-                break;
+                $hasil = 'Operasi tidak valid';
         }
-        return view('calculator', compact('hasil'));
 
+        return view('calculator', compact('hasil', 'angka1', 'angka2', 'operasi'));
     }
 }
